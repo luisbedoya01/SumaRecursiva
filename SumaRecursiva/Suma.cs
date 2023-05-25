45f9d6ebda84ddcf51982afcb0715f73bdf32910
luisbedoya01 <luisbedoya72@gmail.com>
2023-05-24 21:04:28 -0500

    Agregar archivos de proyecto.

---
+++ SumaRecursiva/Suma.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaRecursiva
+{
+    class Suma
+    {
+        static int sumaRecursiva(List<int> lista, int indice)
+        {
+            if (indice == lista.Count) {
+                return 0;
+            }
+            return lista[indice] + sumaRecursiva(lista, indice + 1);
+        }
+
+
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() {1,2,3,4,5,6};
+            int suma = sumaRecursiva(list, 0);
+            Console.WriteLine("El resultado de la suma de los elementos de la lista es: " + suma);
+
+        }
+    }
+}
